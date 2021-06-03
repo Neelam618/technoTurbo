97c523c207ddbc9a4e57db9f6b8444a48c0c2cfb
Neelam Chavan <neelamchavan410@gmail.com>
2021-06-03 17:03:39 +0530

    hero spacing added

---
+++ pages/services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function services() {
+    return (
+        <div className="container px-4">
+            <h2 className="text-5xl font-bold">How we can Help you?</h2>
+            <p>We create Apps, Software, ERP, analyze your buiness data and do automation to help in ease of
+                doing business for B2B and B2C customers.</p>
+        </div>
+    )
+}
