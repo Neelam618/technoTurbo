6f7f0eaf2caae890c365198ab0d6996b34b575b3
Neelam Chavan <neelamchavan410@gmail.com>
2021-06-06 16:26:55 +0530

    features added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        '35': '35%'
+      },
+    },
   },
   variants: {
     display: ['responsive', 'group-hover', 'group-focus'],
