494e83c613eff6858a5d19689c5f4f66efbb65ed
Neelam Chavan <neelamchavan410@gmail.com>
2021-06-06 14:47:35 +0530

    hero section content

--- pages/services.js
+++ pages/services.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function services() {
     return (
-        <div className="container px-4">
-            <h2 className="text-5xl font-bold">How we can Help you?</h2>
+        <div className="container px-4 my-44">
             <p>We create Apps, Software, ERP, analyze your buiness data and do automation to help in ease of
-                doing business for B2B and B2C customers.</p>
+                doing business for B2B and B2C customers.
+            </p>
+            <h2 className="text-5xl font-bold">How we can Help you?</h2>
+            <p>We help you in business growth by creating Apps, Web, Software, ERP and Analytics </p>
         </div>
     )
 }
